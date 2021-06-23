de2080b7124345578c9b9508cf71d765be5542ad
Luca Pinelli <pinelli.luca@gmail.com>
2021-06-23 15:46:35 +0200

    license example

--- README.md
+++ README.md
@@ -15,9 +15,9 @@ Tested using Node.js v12.16.3 and Node.js v14.16.0.
 To generate the certificates you can execute:
 
 ```sh
-openssl genrsa -out ./openssl/master/privkey.pem 2048
-openssl rsa -in ./openssl/master/privkey.pem -pubout -out ./openssl/master/pubkey.pem
+openssl genrsa -out ./openssl/anna/privkey.pem 2048
+openssl rsa -in ./openssl/anna/privkey.pem -pubout -out ./openssl/anna/pubkey.pem
 
-openssl genrsa -out ./openssl/client/privkey.pem 2048
-openssl rsa -in ./openssl/client/privkey.pem -pubout -out ./openssl/client/pubkey.pem
+openssl genrsa -out ./openssl/luca/privkey.pem 2048
+openssl rsa -in ./openssl/luca/privkey.pem -pubout -out ./openssl/luca/pubkey.pem
 ```
---
+++ src/license.js
@@ -0,0 +1,39 @@
+const crypto = require('crypto')
+const fs = require('fs')
+const { promisify } = require('util')
+
+const readFile = promisify(fs.readFile)
+
+const main = async () => {
+  const privKey = await readFile('./openssl/luca/privkey.pem', 'utf8')
+  console.log('$$$ privKey', privKey)
+  const pubKey = await readFile('./openssl/luca/pubkey.pem', 'utf8')
+  console.log('$$$ pubKey', pubKey)
+
+  const license = {
+    expiryDate: '2022-06-30T23:59:59.999Z',
+    profile: 'enterprise'
+  }
+
+  const encrypted = crypto
+    .privateEncrypt(privKey, Buffer.from(JSON.stringify(license)))
+    .toString('base64')
+
+  console.log('$$$ encrypted', encrypted)
+
+  const decrypted = JSON.parse(crypto
+    .publicDecrypt(pubKey, Buffer.from(encrypted, 'base64'))
+    .toString('utf8'))
+
+  console.log('$$$ decrypted', decrypted)
+
+}
+
+main()
+.then(() => {
+  console.log('program terminated.')
+})
+.catch(e => {
+  console.log('ERROR:', e)
+  process.exit(2021)
+})
